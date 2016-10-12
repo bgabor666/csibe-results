be4d5f624e3d47527efede3648ac62ad994b763d
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-10-12 14:15:38 +0000

    Add target names and flags to the JSON data

--- bin/csibe_csv_to_json.py
+++ bin/csibe_csv_to_json.py
@@ -9,16 +9,25 @@ def copy_csibe_results_from_csv_to_json(csv_path, json_path):
     with open(csv_path) as csv_file:
         csv_contents = csv_file.read().splitlines()
 
+    csv_path_basename = os.path.basename(csv_path)
+    print csv_path_basename[11:-20]
+    target_name = csv_path_basename[11:-20]
+
     llvm_revision = csv_contents[4].split(',')[1]
-    build_date = os.path.basename(csv_path)[:10]
+    build_date = csv_path_basename[:10]
 
     if not os.path.isfile(json_path):
-        json_data = {'revisions': [], 'date': build_date}
+        json_data = {'revisions': {}, 'date': build_date}
     else:
         with open(json_path) as json_file:
             json_data = json.load(json_file)
     
-    json_data['revisions'].append({llvm_revision: []})
+    if llvm_revision not in json_data['revisions']:
+        json_data['revisions'][llvm_revision] = {}
+
+    target_flags = csv_contents[1].split(',')[1].split()
+    target_details = {'flags': target_flags}
+    json_data['revisions'][llvm_revision][target_name] = target_details
 
     print json.dumps(json_data, sort_keys=True, indent=4, separators=(',',':'))
 
