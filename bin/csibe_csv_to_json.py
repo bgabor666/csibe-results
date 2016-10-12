90e123feeb9c61428e56920ce7a4042ed61a1ba4
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-10-12 17:40:06 +0000

    Add project result dictionary to the JSON data

--- bin/csibe_csv_to_json.py
+++ bin/csibe_csv_to_json.py
@@ -10,11 +10,19 @@ def copy_csibe_results_from_csv_to_json(csv_path, json_path):
         csv_contents = csv_file.read().splitlines()
 
     csv_path_basename = os.path.basename(csv_path)
-    print csv_path_basename[11:-20]
     target_name = csv_path_basename[11:-20]
 
     llvm_revision = csv_contents[4].split(',')[1]
     build_date = csv_path_basename[:10]
+
+    result_dict = {}
+    result_list = [element.split(',') for element in csv_contents[6:]]
+    for split_result in result_list:
+        project_name = split_result[0]
+        project_result = int(split_result[2])
+        if project_name not in result_dict:
+            result_dict[project_name] = 0
+        result_dict[project_name] += project_result
 
     if not os.path.isfile(json_path):
         json_data = {'revisions': {}, 'date': build_date}
@@ -26,7 +34,7 @@ def copy_csibe_results_from_csv_to_json(csv_path, json_path):
         json_data['revisions'][llvm_revision] = {}
 
     target_flags = csv_contents[1].split(',')[1].split()
-    target_details = {'flags': target_flags}
+    target_details = {'flags': target_flags, 'projects': result_dict}
     json_data['revisions'][llvm_revision][target_name] = target_details
 
     print json.dumps(json_data, sort_keys=True, indent=4, separators=(',',':'))
